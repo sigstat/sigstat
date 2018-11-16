c97cf6573d1faf9e06bec50cd92e5ca67fff7b86
Lénárt Győző Levente <3868156+fabledpig@users.noreply.github.com>
2018-11-16 10:51:49 +0100

    Create test for FeatureAttribute class

---
+++ test/SigStat.Common.Test/Model/FeatureAttributeTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SigStat.Common;
+
+namespace SigStat.Common.Test.Model
+{
+    [TestClass]
+    public class FeatureAttributeTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            const string featureKey = "Loop";
+            var fa = new FeatureAttribute("Loop");
+
+            Assert.AreEqual(featureKey, fa.FeatureKey);
+        }
+
+    }
+}
