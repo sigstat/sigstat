bba1d86072de14a0d7390dc26417ff7ff3b1551c
Péter Nyakas <nyakaspeter@outlook.com>
2018-12-02 22:01:39 +0100

    Unit test samples for Algorithms

---
+++ test/SigStat.Common.Test/Algorithms/DtwTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigStat.Common.Test.Algorithms
+{
+    [TestClass]
+    public class DtwTests
+    {
+
+        [TestMethod]
+        public void TestCompute()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDistance()
+        {
+
+        }
+    }
+}
---
+++ test/SigStat.Common.Test/Features/FeaturesTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigStat.Common.Test.Features
+{
+    class FeaturesTests
+    {
+    }
+}
