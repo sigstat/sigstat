6e10d169d345930eb2a88f21832465c2fc5f1d4c
Tomi <hargitomi97@gmail.com>
2019-03-01 10:52:08 +0100

    Revert "add comments for docu testing"
    This reverts commit 1bdb8a2e3b00dda1b6c7069956e2cd76d5115a5a.

--- src/SigStat.Cli/Program.cs
+++ src/SigStat.Cli/Program.cs
@@ -1,99 +1,99 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Drawing;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
 
-////Ez is lehetne helyette: https://github.com/natemcmaster/CommandLineUtils
-//using Microsoft.Extensions.CommandLineUtils;
+//Ez is lehetne helyette: https://github.com/natemcmaster/CommandLineUtils
+using Microsoft.Extensions.CommandLineUtils;
 
-//using SigStat.Common;
-//using SigStat.Common.Loaders;
-//using SigStat.Common.Helpers;
-//using System.IO;
+using SigStat.Common;
+using SigStat.Common.Loaders;
+using SigStat.Common.Helpers;
+using System.IO;
 
-//namespace SigStat.Cli
-//{
-//    //CLI guidelines:
-//    //Do use Commands when semantically identifying an action
-//    //Do use Options to enable configuration to a specific Command
-//    //Favor verb for command name
-//    //Favor adjective or noun for option name
+namespace SigStat.Cli
+{
+    //CLI guidelines:
+    //Do use Commands when semantically identifying an action
+    //Do use Options to enable configuration to a specific Command
+    //Favor verb for command name
+    //Favor adjective or noun for option name
 
-//    class Program
-//    {
-//        static Logger cliLogger;
+    class Program
+    {
+        static Logger cliLogger;
      
-//        static void Main(string[] args)
-//        {
-//            var app = new CommandLineApplication();
-//            app.Name = "sigstat";//Root Command
-//            app.HelpOption("-?|-h|--help");
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Name = "sigstat";//Root Command
+            app.HelpOption("-?|-h|--help");
 
-//            app.OnExecute(() => {
-//                //ez akkor fut le, amikor a root commandot argument nelkul hivjuk
-//                Console.WriteLine("sigstat cli");
-//                return 0;
-//            });
+            app.OnExecute(() => {
+                //ez akkor fut le, amikor a root commandot argument nelkul hivjuk
+                Console.WriteLine("sigstat cli");
+                return 0;
+            });
 
-//            app.Command("log", (command) =>
-//            {
-//                command.Description = "Initialize a logger.";
-//                command.HelpOption("-?|-h|--help");
+            app.Command("log", (command) =>
+            {
+                command.Description = "Initialize a logger.";
+                command.HelpOption("-?|-h|--help");
 
-//                var levelArg = command.Argument("[level]", "Level of logging.");//TODO: felsorolni a szinteket a help ben.
-//                var pathArg = command.Argument("[path]", "File path to log messages.");
+                var levelArg = command.Argument("[level]", "Level of logging.");//TODO: felsorolni a szinteket a help ben.
+                var pathArg = command.Argument("[path]", "File path to log messages.");
 
-//                var helloOpt = command.Option("-he|--hello", "Option to say Hello World.", CommandOptionType.NoValue);
-//                var displayOpt = command.Option("-d|--display", "Option to display log messages on console.", CommandOptionType.NoValue);
+                var helloOpt = command.Option("-he|--hello", "Option to say Hello World.", CommandOptionType.NoValue);
+                var displayOpt = command.Option("-d|--display", "Option to display log messages on console.", CommandOptionType.NoValue);
 
-//                command.OnExecute(() =>
-//                {
-//                    //init logger
-//                    string path = pathArg.Value ?? $"{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.log";
-//                    FileStream fs = new FileStream(path, FileMode.Create);
-//                    if (displayOpt.HasValue())
-//                        cliLogger = new Logger(LogLevel.Debug, fs, LogConsole);//TODO: levelArg alapjan beallitani
-//                    else
-//                        cliLogger = new Logger(LogLevel.Debug, fs, null);
+                command.OnExecute(() =>
+                {
+                    //init logger
+                    string path = pathArg.Value ?? $"{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.log";
+                    FileStream fs = new FileStream(path, FileMode.Create);
+                    if (displayOpt.HasValue())
+                        cliLogger = new Logger(LogLevel.Debug, fs, LogConsole);//TODO: levelArg alapjan beallitani
+                    else
+                        cliLogger = new Logger(LogLevel.Debug, fs, null);
 
-//                    if (helloOpt.HasValue())
-//                        cliLogger.Info(cliLogger, "Hello World!");
-//                    return 0;
-//                });
-//            });
+                    if (helloOpt.HasValue())
+                        cliLogger.Info(cliLogger, "Hello World!");
+                    return 0;
+                });
+            });
 
-//            //TODO: add more commands
-//            //valahogy lehetne kulon .cs be irni a commandokat, mint dotnet-cli ben
+            //TODO: add more commands
+            //valahogy lehetne kulon .cs be irni a commandokat, mint dotnet-cli ben
 
-//            app.Execute(args);
-//        }
+            app.Execute(args);
+        }
 
-//        public static void LogConsole(LogLevel l, string message)
-//        {
-//            switch (l)
-//            {
-//                case LogLevel.Fatal:
-//                case LogLevel.Error:
-//                    Console.ForegroundColor = ConsoleColor.Red;
-//                    break;
-//                case LogLevel.Warn:
-//                    Console.ForegroundColor = ConsoleColor.Yellow;
-//                    break;
-//                case LogLevel.Info:
-//                    Console.ForegroundColor = ConsoleColor.White;
-//                    break;
-//                case LogLevel.Debug:
-//                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-//                    break;
-//                default:
-//                    break;
-//            }
-//            Console.WriteLine(message);
-//            Console.ForegroundColor = ConsoleColor.Gray;
-//        }
+        public static void LogConsole(LogLevel l, string message)
+        {
+            switch (l)
+            {
+                case LogLevel.Fatal:
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogLevel.Warn:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
 
 
-//    }
-//}
+    }
+}
 
