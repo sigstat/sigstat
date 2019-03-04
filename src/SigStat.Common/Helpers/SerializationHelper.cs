042d1e9b97a0daa600e4c6c0f46c6b3f22432774
BenceKovari <kovari@aut.bme.hu>
2019-03-04 15:32:43 +0100

    serializer előkészítés

---
+++ src/SigStat.Common/Helpers/SerializationHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigStat.Common.Helpers
+{
+    public class SerializationHelper
+    {
+        public static T Deserialize<T>(string s) where T:new()
+        {
+            return new T();
+        }
+
+        public static string Serialize<T>(T o)
+        {
+            return "";
+        }
+    }
+}
