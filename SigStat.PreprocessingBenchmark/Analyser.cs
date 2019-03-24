21000635d9191cc3317a11ee2f919897ea03f7e1
BenceKovari <kovari@aut.bme.hu>
2019-03-24 09:27:48 +0100

    error handling

--- SigStat.PreprocessingBenchmark/Analyser.cs
+++ SigStat.PreprocessingBenchmark/Analyser.cs
@@ -33,6 +33,8 @@ namespace SigStat.PreprocessingBenchmark
             public string Features { get; set; }
             public string Distance { get; set; }
 
+            public string Error { get; set; }
+
             public double FRR { get; set; }
             public double FAR { get; set; }
             public double AER { get; set; }
@@ -40,12 +42,14 @@ namespace SigStat.PreprocessingBenchmark
         internal static async Task RunAsync()
         {
             var reportFiles = Directory.EnumerateFiles(Program.OutputDirectory, "*.xlsx").ToList();
+            reportFiles.Sort();
 
             var reports = new ConcurrentBag<ReportLine>();
-            Parallel.ForEach(reportFiles, rf =>
+            Parallel.ForEach(reportFiles,new ParallelOptions {MaxDegreeOfParallelism=16 }, rf =>
             {
+                if (reports.Count % 100 == 0)
+                    Console.WriteLine(DateTime.Now+": "+ reports.Count + "/" + reportFiles.Count);
                 reports.Add(LoadReportLine(rf));
-                Console.WriteLine(reports.Count + "/" + reportFiles.Count);
 
             });
 
@@ -61,33 +65,46 @@ namespace SigStat.PreprocessingBenchmark
 
         private static ReportLine LoadReportLine(string filename)
         {
-            using (var p = new ExcelPackage(new FileInfo(filename)))
+            try
             {
-                var summary = p.Workbook.Worksheets["Summary"];
-                var result =  new ReportLine();
-                result.FAR = summary.Cells["I10"].GetValue<double>();
-                result.FRR = summary.Cells["I11"].GetValue<double>();
-                result.AER = summary.Cells["I12"].GetValue<double>();
-                result.Key = Path.GetFileNameWithoutExtension(filename);
-
-                for (int row = 10; row <= 19; row++)
+                using (var p = new ExcelPackage(new FileInfo(filename)))
                 {
-                    string key = summary.Cells[row, 5].GetValue<string>();
-                    string value = summary.Cells[row, 6].GetValue<string>();
-                    typeof(ReportLine).GetProperty(key).SetValue(result, value);
+                    var summary = p.Workbook.Worksheets["Summary"];
+                    var result = new ReportLine();
+                    result.FAR = summary.Cells["I10"].GetValue<double>();
+                    result.FRR = summary.Cells["I11"].GetValue<double>();
+                    result.AER = summary.Cells["I12"].GetValue<double>();
+                    result.Key = Path.GetFileNameWithoutExtension(filename);
+
+                    for (int row = 10; row <= 19; row++)
+                    {
+                        string key = summary.Cells[row, 5].GetValue<string>();
+                        string value = summary.Cells[row, 6].GetValue<string>();
+                        typeof(ReportLine).GetProperty(key).SetValue(result, value);
+
+                    }
+
+                    for (int row = 11; row <= 13; row++)
+                    {
+                        string key = summary.Cells[row, 2].GetValue<string>().Replace(":", "");
+                        string value = summary.Cells[row, 3].GetValue<string>();
+                        typeof(ReportLine).GetProperty(key).SetValue(result, value);
+
+                    }
+
+                    return result;
 
                 }
-
-                for (int row = 11; row <= 13; row++)
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(Path.GetFileNameWithoutExtension(filename));
+                Console.WriteLine(exc.Message);
+                return new ReportLine
                 {
-                    string key = summary.Cells[row, 2].GetValue<string>().Replace(":","");
-                    string value = summary.Cells[row, 3].GetValue<string>();
-                    typeof(ReportLine).GetProperty(key).SetValue(result, value);
-
-                }
-
-                return result;
-
+                    Key = Path.GetFileNameWithoutExtension(filename),
+                    Error = exc.Message
+                };
             }
         }
     }
