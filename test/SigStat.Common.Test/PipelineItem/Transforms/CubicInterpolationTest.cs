ddc930eeb1e9bab5658b3fc1e30416cd36c7a301
Bedő Bence <bence9705@gmail.com>
2019-05-07 01:18:30 +0200

    Cubic and Linear interpolation Test expanded

--- test/SigStat.Common.Test/PipelineItem/Transforms/CubicInterpolationTest.cs
+++ test/SigStat.Common.Test/PipelineItem/Transforms/CubicInterpolationTest.cs
@@ -9,8 +9,36 @@ namespace SigStat.Common.Test.PipelineItem.Transforms
     [TestClass]
     public class CubicInterpolationTest
     {
+        CubicInterpolation ci = new CubicInterpolation();
+        CubicInterpolation ci2 = new CubicInterpolation();
+
         [TestMethod]
-        public void TestGetValue()
+        public void TestGetValueExceptionHandling()
+        {
+            try
+            {
+                double resultException = ci.GetValue(2.0);
+                Assert.Fail("no exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("List of timestamps is null", ex.Message);
+            }
+
+            try
+            {
+                ci.TimeValues = new List<double> { 1.0, 2.0, 3.0 };
+                double resultException = ci.GetValue(2.0);
+                Assert.Fail("no exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("List of feature values is null", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetValueContains()
         {
             List<double> timeValues = new List<double>();
             timeValues.Add(2.0);
@@ -20,16 +48,46 @@ namespace SigStat.Common.Test.PipelineItem.Transforms
             featueValues.Add(3.0);
             featueValues.Add(4.0);
             featueValues.Add(5.0);
-            
-            CubicInterpolation ci = new CubicInterpolation();
+
+
+            //timeValues contains timestamps
             ci.TimeValues = timeValues;
             ci.FeatureValues = featueValues;
 
-            double result = ci.GetValue(2.0);
-            double expected = 3.0;
+            double result1 = ci.GetValue(2.0);
+            double expected1 = 3.0;
+            Assert.AreEqual(expected1, result1);
 
-            Assert.AreEqual(expected, result);
+
+
         }
-        
+
+        [TestMethod]
+        public void TestGetValueDoesNotContains()
+        {
+            List<double> timeValues = new List<double>();
+            timeValues.Add(2.0);
+            timeValues.Add(4.0);
+            timeValues.Add(6.0);
+            timeValues.Add(8.0);
+            timeValues.Add(10.0);
+            timeValues.Add(12.0);
+            List<double> featueValues = new List<double>();
+            featueValues.Add(2.0);
+            featueValues.Add(4.0);
+            featueValues.Add(6.0);
+            featueValues.Add(8.0);
+            featueValues.Add(10.0);
+            featueValues.Add(12.0);
+
+            //timeValues does not contains timestamps
+            ci2.TimeValues = timeValues;
+            ci2.FeatureValues = featueValues;
+
+            double result2 = ci2.GetValue(3.0);
+            double expected2 = 3.0;
+            Assert.AreEqual(expected2, result2);
+
+        }
     }
 }
--- test/SigStat.Common.Test/PipelineItem/Transforms/LinearInterpolationTest.cs
+++ test/SigStat.Common.Test/PipelineItem/Transforms/LinearInterpolationTest.cs
@@ -9,26 +9,67 @@ namespace SigStat.Common.Test.PipelineItem.Transforms
     [TestClass]
     public class LinearInterpolationTest
     {
+        LinearInterpolation li = new LinearInterpolation();
+        LinearInterpolation li2 = new LinearInterpolation();
+
+        [TestMethod]
+        public void TestGetValueExceptionHandling()
+        {
+            try
+            {
+                double resultException = li.GetValue(2.0);
+                Assert.Fail("no exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("List of timestamps is null", ex.Message);
+            }
+
+            try
+            {
+                li.TimeValues = new List<double> { 1.0, 2.0, 3.0 };
+                double resultException = li.GetValue(2.0);
+                Assert.Fail("no exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("List of feature values is null", ex.Message);
+            }
+        }
+
         [TestMethod]
         public void TestGetValue()
         {
-            List<double> timeValues = new List<double>();
-            timeValues.Add(2.0);
-            timeValues.Add(3.0);
-            timeValues.Add(4.0);
-            List<double> featueValues = new List<double>();
-            featueValues.Add(3.0);
-            featueValues.Add(4.0);
-            featueValues.Add(5.0);
+            List<double> timeValues = new List<double>() { 2.0, 3.0, 4.0};
+            
+            List<double> featueValues = new List<double>() { 3.0,4.0,5.0};
+            
+            LinearInterpolation li = new LinearInterpolation();
+            li.TimeValues = timeValues;
+            li.FeatureValues = featueValues;
 
-            LinearInterpolation ci = new LinearInterpolation();
-            ci.TimeValues = timeValues;
-            ci.FeatureValues = featueValues;
-
-            double result = ci.GetValue(2.0);
+            double result = li.GetValue(2.0);
             double expected = 3.0;
 
             Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestGetValueDoesNotContains()
+        {
+            List<double> timeValues = new List<double>() { 2.0,4.0,6.0,8.0,10.0,12.0 };
+            
+            List<double> featueValues = new List<double>() { 2.0, 4.0, 6.0, 8.0, 10.0, 12.0 };
+            
+            //timeValues does not contains timestamps
+            li2.TimeValues = timeValues;
+            li2.FeatureValues = featueValues;
+
+            double result2 = li2.GetValue(3.0);
+            double expected2 = 3.0;
+            
+            Assert.AreEqual(expected2, result2);
+
         }
     }
 }
