1a7e79b15724cfc2321446b05af2a07762dec0e8
Tomi <hargitomi97@gmail.com>
2019-05-11 19:00:00 +0200

    Update table

--- docs/site/scripts/table.js
+++ docs/site/scripts/table.js
@@ -1,28 +1,23 @@
 $(function () {
   $.ajax({
     type: 'GET',
-    //url: 'https://api.myjson.com/bins/6o6l0',
-    url: 'https://api.myjson.com/bins/16gc1g',
+    // url: 'https://api.myjson.com/bins/z9efy', // with columnType
+    // url: 'https://api.myjson.com/bins/17w6pq', // without columnType
+    url: 'https://raw.githubusercontent.com/sigstat/sigstat/596ff6d7b14de0125123704ed2744754f2aae67e/docs/site/scripts/Results.json', // from GH
     dataType: 'json',
     success: function (data) {
       var length = Object.keys(data.databases).length;
-      var th = "<th class=\"align-middle\"";
+      var th = "<th class=\"rotate\">";
       var th_end = "</th>";
-      var rowspan2 = " rowspan=\"2\">";
-      //console.log(length);
 
-      $("#asd").append(th + rowspan2 + "Verifier's Name" + th_end);
+      $("#header").append("<th class=\"align-middle\">" + "Verifier's" + "<br>"+ "Name" + th_end);
+      $("#header").append("<th class=\"align-middle\">" + "Average" + th_end);
+      $("#header").append("<th class=\"align-middle\">" + "Average" + "<br>" + "Core" + th_end);
       for (var i = 0; i < length; i++) {
-        $("#asd").append("<th colspan=\"3\">" + "<span>" + data.databases[i].Name + "</span>" + "</th>");
-      }
-      for (var i = 0; i < length; i++) {
-        $("#asd2").append("<th><em>" + "AER" + "</em></th>");
-        $("#asd2").append("<th><em>" + "FAR" + "</em></th>");
-        $("#asd2").append("<th><em>" + "FRR" + "</em></th>");
+        $("#header").append(th + "<div>" + "<span>" + data.databases[i].Name + "</div>" + "</span>" + th_end);
       }
 
       var res_length = Object.keys(data.results).length;
-      console.log(res_length);
 
       var databaseNames = Object.keys(data.results[0]);
       databaseNames.splice(0, 3);
@@ -30,18 +25,32 @@ $(function () {
       var dynamic = "";
 
       for (var i = 0; i < res_length; i++) {
-        dynamic += "<tr>" + "<td>" + data.results[i].verifierName + "</td>"
+        dynamic += "<tr>" + "<td class=\"verName\">" + data.results[i].verifierName + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + (data.results[i]["Average"].AER).toFixed(2) + "%"+ "</p>" + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + data.results[i]["Average Core"].AER.toFixed(2) + "%"+ "</p>" + "</td>";
         for (var j = 0; j < length; j++) {
-          dynamic += "<td>" + (data.results[i][databaseNames[j]].AER == null ? "?" : data.results[i][databaseNames[j]].AER) + "</td>" +
-            "<td>" + (data.results[i][databaseNames[j]].FAR == null ? "?" : data.results[i][databaseNames[j]].FAR) + "</td>" +
-            "<td>" + (data.results[i][databaseNames[j]].FRR == null ? "?" : data.results[i][databaseNames[j]].FRR) + "</td>";
+          dynamic += "<td>" + "<em>" + (data.results[i][databaseNames[j]].AER == null ? " " : (data.results[i][databaseNames[j]].AER).toFixed(2) + "%") + "</em>" + "</td>";
         }
         dynamic += "</tr>";
 
       }
-
-
       $("#content").append(dynamic);
+
+      var IsValueSummary = data.columnType;
+      if (IsValueSummary === 'summary'){
+        var bar = newSheet();
+        bar.insertRule("p.summary {color: white;}",0);
+        bar.insertRule("p.summary {background-color: chocolate;}",0);
+      }
+
+      function newSheet() {
+        var style = document.createElement("style");
+       
+        style.appendChild(document.createTextNode(""));
+        document.head.appendChild(style);
+       
+        return style.sheet;
+       };
 
       $("body").on('mouseover', 'span:not(.tooltipstered)', function () {
         var tooltipInstance = null;
@@ -50,28 +59,31 @@ $(function () {
         var eol = "<br>";
 
         var first = data.databases.find(asd => asd.Name === temp);
-        //var count = Object.keys(first).length - 1;
-        //console.log(count);
         var test = "";
 
-        for (var i = 1; i <= 6; i++) {
+        for (var i = 1; i <= 5; i++) {
           test += Object.keys(first)[i] + sep + first[Object.keys(first)[i]] + eol
         }
+        test += Object.keys(first)[6] + sep + "<a href=" + first[Object.keys(first)[i]] + " target=\"_blank\">" + first[Object.keys(first)[i]] + "</a>";
+
 
         tooltipInstance = $(this).tooltipster({
           contentAsHTML: true,
           content: test,
-          delay: 0,
-          theme: 'tooltipster-punk'
-        });
+          delay: 100,
+          theme: 'tooltipster-light',
+          interactive: true
+        })
 
         tooltipInstance.tooltipster('open');
       })
-      $("table tbody tr td:first-child").on('mouseover', function () {
+
+      $("#myTable tbody tr td:first-child").on('mouseover', function () {
         var tooltipInstance = null;
         var temp = ($(this).html());
         var sep = ": ";
         var eol = "<br>";
+
 
         var first = data.results.find(asd => asd.verifierName === temp);
         var test = "";
@@ -84,29 +96,80 @@ $(function () {
           contentAsHTML: true,
           content: test,
           delay: 0,
-          theme: 'tooltipster-punk'
+          theme: 'tooltipster-default',
+          multiple: true
         })
         tooltipInstance.tooltipster('open');
       })
-      $("body").on('mouseover', 'em:not(.tooltipstered)', function () {
-        var temp = ($(this).html());
-        var tooltipInstance = null;
-        var content = null;
 
-        if (temp == "AER") {
-          content = "Average Error Rate";
+      $("body").on('mouseover', 'p:not(.tooltipstered)', function () {
+        var rowData = ($(this).parent().parent().find('td:first').text());
+        var currentColumnIndex = $(this).parent().index();
+
+
+        var temp = data.results.find(asd => asd.verifierName === rowData);
+        var almost = "";
+        if(currentColumnIndex === 1){
+          almost = temp[Object.keys(temp)[Object.keys(temp).length-2]];
         }
-        if (temp == "FAR") {
-          content = "False Acceptance Rate";
+        if(currentColumnIndex === 2){
+          almost = temp[Object.keys(temp)[Object.keys(temp).length-1]];
         }
-        if (temp == "FRR") {
-          content = "False Rejection Rate";
+        
+        var test = "";
+
+        for (var i = 0; i <= 2; i++) {
+          test += Object.keys(almost)[i] + ": " + almost[Object.keys(almost)[i]].toFixed(2) + "%" + "<br>";
         }
+
+        
+
         tooltipInstance = $(this).tooltipster({
           contentAsHTML: true,
-          content: content,
+          content: test,
           delay: 0,
-          theme: 'tooltipster-light'
+          theme: 'tooltipster-noir',
+        })
+
+        tooltipInstance.tooltipster('open');
+      })
+
+      $("body").on('mouseover', 'em:not(.tooltipstered)', function () {
+        var rowData = ($(this).parent().parent().find('td:first').text());
+        var currentColumnIndex = $(this).parent().index() - 3;
+        var db = data.databases[currentColumnIndex];
+        var final = db[Object.keys(db)[0]];
+
+        var temp = data.results.find(asd => asd.verifierName === rowData);
+        var almost = temp[Object.keys(temp)[currentColumnIndex + 3]];
+        
+
+
+        var tooltipInstance = null;
+
+
+        var row = data.results.find(asd => asd.verifierName === rowData);
+        var test = "";
+
+        test += Object.keys(row)[0] + ": " + row[Object.keys(row)[0]] + "<br>";
+        test += "databaseName: " + final + "<br>";
+
+        
+        for (var i = 0; i <= 2; i++) {
+          test += Object.keys(almost)[i] + ": " + almost[Object.keys(almost)[i]].toFixed(2) + "%" + "<br>";
+        }
+        
+        if(Object.keys(almost)[3] === 'Comment'){
+          test += Object.keys(almost)[3] + ": " + almost[Object.keys(almost)[3]];
+        }
+
+
+        tooltipInstance = $(this).tooltipster({
+          contentAsHTML: true,
+          content: test,
+          delay: 0,
+          theme: 'tooltipster-punk'
+
         })
         tooltipInstance.tooltipster('open');
 
