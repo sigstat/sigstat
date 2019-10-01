92ccd944985e21f44109cef769ce4c11822ee8eb
hargitomi97 <36476318+hargitomi97@users.noreply.github.com>
2019-10-01 17:37:18 +0200

    First version of webpage

--- docs/site/scripts/table.js
+++ docs/site/scripts/table.js
@@ -1,179 +1,345 @@
 $(function () {
-  $.ajax({
-    type: 'GET',
-    // url: 'https://api.myjson.com/bins/z9efy', // with columnType example
-    url: 'http://sigstat.org/site/scripts/Results.json', // directly from the site
-    dataType: 'json',
-    success: function (data) {
-      let db_length = Object.keys(data.databases).length;
-      let th = "<th class=\"rotate\">";
-      let th_end = "</th>";
+    var url = "https://cors-anywhere.herokuapp.com/https://github.com/sigstat/sigstat/blob/develop/docs/site/Summary_short.xlsx?raw=true";
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
 
-      $("#header").append("<th class=\"align-middle\">" + "Verifier's" + "<br>" + "Name" + th_end);
-      $("#header").append("<th class=\"align-middle\">" + "Average" + th_end);
-      $("#header").append("<th class=\"align-middle\">" + "Average" + "<br>" + "Core" + th_end);
-      for (let i = 0; i < db_length; i++) {
-        $("#header").append(th + "<div>" + "<span>" + data.databases[i].Name + "</div>" + "</span>" + th_end);
-      }
+    oReq.onload = function (e) {
+        var arraybuffer = oReq.response;
 
-      let res_length = Object.keys(data.results).length;
+        /* convert data to binary string */
+        var data = new Uint8Array(arraybuffer);
+        var arr = new Array();
+        for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+        var bstr = arr.join("");
 
-      let databaseNames = Object.keys(data.results[0]);
-      databaseNames.splice(0, 3);
+        /* Call XLSX */
+        var workbook = XLSX.read(bstr, { type: "binary" });
 
-      let dynamic = "";
+        /* DO SOMETHING WITH workbook HERE */
+        var first_sheet_name = workbook.SheetNames[0];
+        /* Get worksheet */
+        var worksheet = workbook.Sheets[first_sheet_name];
+        var data = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+        // console.log(data);
 
-      for (let i = 0; i < res_length; i++) {
-        dynamic += "<tr>" + "<td class=\"verName\">" + data.results[i].verifierName + "</td>";
-        dynamic += "<td>" + "<p class=\"summary\">" + (data.results[i]["Average"].AER).toFixed(2) + "%" + "</p>" + "</td>";
-        dynamic += "<td>" + "<p class=\"summary\">" + data.results[i]["Average Core"].AER.toFixed(2) + "%" + "</p>" + "</td>";
-        for (let j = 0; j < db_length; j++) { // if AER is missing due to exception
-          dynamic += "<td>" + "<em>" + (data.results[i][databaseNames[j]].AER == null ? " " : (data.results[i][databaseNames[j]].AER).toFixed(2) + "%") + "</em>" + "</td>";
+
+
+        var th = "<th class=\"rotate\">";
+        var th_end = "</th>";
+        var a = new Array();
+        for (var k in data) {
+            a.push((data[k].Database));
         }
-        dynamic += "</tr>";
+        var uniqueArray = new Array();
+        uniqueArray = a.filter(onlyUnique);
+        //console.log(uniqueArray.toString());
+        var uniqueDatabasesCount = uniqueArray.length;
+        //console.log(uniqueDatabasesCount);
 
-      }
-      $("#content").append(dynamic);
 
-      let IsValueSummary = data.columnType;
-      if (IsValueSummary === 'summary') {
-        let bar = newSheet();
-        bar.insertRule("p.summary {color: white;}", 0);
-        bar.insertRule("p.summary {background-color: chocolate;}", 0);
-      }
+        $("#header").append("<th class=\"align-middle\">" + "Verifier's" + "<br>" + "Name" + th_end);
+        $("#header").append("<th class=\"align-middle\">" + "average" + th_end);
 
-      function newSheet() {
-        let style = document.createElement("style");
-
-        style.appendChild(document.createTextNode(""));
-        document.head.appendChild(style);
-
-        return style.sheet;
-      };
-
-      // table header
-      $("body").on('mouseover', 'span:not(.tooltipstered)', function () {
-        let tooltipInstance = null;
-        let temp = ($(this).html());
-        let sep = ": ";
-        let eol = "<br>";
-
-        let singleDB = data.databases.find(asd => asd.Name === temp);
-        let headers = "";
-
-        for (var i = 1; i <= 5; i++) {
-          headers += Object.keys(singleDB)[i] + sep + singleDB[Object.keys(singleDB)[i]] + eol
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            $("#header").append(th + "<div>" + "<span>" + uniqueArray[i] + "</div>" + "</span>" + th_end);
         }
-        headers += Object.keys(singleDB)[6] + sep + "<a href=" + singleDB[Object.keys(singleDB)[i]] + " target=\"_blank\">" + singleDB[Object.keys(singleDB)[i]] + "</a>";
+
+        var Translation;
+        var Scaling;
+        var ResamplingTypeFilter;
+        var Features;
+
+        //1. sor
+
+        Translation = "CogToOriginXY";
+        Scaling = "None";
+        ResamplingTypeFilter = "None";
+        Features = "XY";
+
+        var x = data.filter(function (el) {
+            return el.Translation == Translation &&
+                el.Scaling == Scaling &&
+                el.ResamplingType_Filter == ResamplingTypeFilter &&
+                el.Features == Features;
+        });
+
+        //console.log(x);
+        var dynamic;
+        var averageAER = 0;
+        var averageFAR = 0;
+        var averageFRR = 0;
+
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            averageAER += x[i].AER * 100;
+        }
+        //console.log(averageAER);
+        averageAER = (averageAER / uniqueDatabasesCount).toFixed(2);
+
+        dynamic += "<tr>" + "<td class=\"verName\">" + Translation.substr(0, Translation.length - 2) + ", " + Features + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + averageAER + "%" + "</p>" + "</td>";
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            dynamic += "<td>" + "<em>" + (x[i].AER * 100).toFixed(2) + "%" + "</em>" + "</td>";
+        }
 
 
-        tooltipInstance = $(this).tooltipster({
-          contentAsHTML: true,
-          content: headers,
-          delay: 100,
-          theme: 'tooltipster-light',
-          interactive: true
+        //2. sor
+
+        Translation = "None";
+        var translationAppear;
+        if (Translation = "None") {
+            translationAppear = "Normalization";
+        }
+        Scaling = "X01Y01";
+        ResamplingTypeFilter = "None";
+        Features = "XY";
+
+        x = data.filter(function (el) {
+            return el.Translation == Translation &&
+                el.Scaling == Scaling &&
+                el.ResamplingType_Filter == ResamplingTypeFilter &&
+                el.Features == Features;
+        });
+
+        averageAER = 0;
+        averageFAR = 0;
+        averageFRR = 0;
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            averageAER += x[i].AER * 100;
+        }
+        averageAER = (averageAER / uniqueDatabasesCount).toFixed(2);
+
+        dynamic += "<tr>" + "<td class=\"verName\">" + translationAppear + ", " + Features + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + averageAER + "%" + "</p>" + "</td>";
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            dynamic += "<td>" + "<em>" + (x[i].AER * 100).toFixed(2) + "%" + "</em>" + "</td>";
+        }
+
+        // 3. sor
+
+        Translation = "CogToOriginXY";
+        Scaling = "None";
+        ResamplingTypeFilter = "None";
+        Features = "XYP";
+
+        x = data.filter(function (el) {
+            return el.Translation == Translation &&
+                el.Scaling == Scaling &&
+                el.ResamplingType_Filter == ResamplingTypeFilter &&
+                el.Features == Features;
+        });
+
+
+
+        averageAER = 0;
+        averageFAR = 0;
+        averageFRR = 0;
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            averageAER += x[i].AER * 100;
+            averageFAR += x[i].FAR * 100;
+        }
+        averageAER = (averageAER / uniqueDatabasesCount).toFixed(2);
+        averageFAR = (averageFAR / uniqueDatabasesCount).toFixed(2);
+
+        dynamic += "<tr>" + "<td class=\"verName\">" + Translation.substr(0, Translation.length - 2) + ", " + Features + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + averageAER + "%" + "</p>" + "</td>";
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            dynamic += "<td>" + "<em>" + (x[i].AER * 100).toFixed(2) + "%" + "</em>" + "</td>";
+        }
+
+        // 4. sor
+
+        Translation = "None";
+        Scaling = "X01Y01";
+        ResamplingTypeFilter = "None";
+        Features = "XYP";
+
+        x = data.filter(function (el) {
+            return el.Translation == Translation &&
+                el.Scaling == Scaling &&
+                el.ResamplingType_Filter == ResamplingTypeFilter &&
+                el.Features == Features;
+        });
+
+
+
+        averageAER = 0;
+        averageFAR = 0;
+        averageFRR = 0;
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            averageAER += x[i].AER * 100;
+            averageFAR += x[i].FAR * 100;
+        }
+        averageAER = (averageAER / uniqueDatabasesCount).toFixed(2);
+        averageFAR = (averageFAR / uniqueDatabasesCount).toFixed(2);
+
+        dynamic += "<tr>" + "<td class=\"verName\">" + translationAppear + ", " + Features + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + averageAER + "%" + "</p>" + "</td>";
+        for (var i = 0; i < uniqueDatabasesCount; i++) {
+            dynamic += "<td>" + "<em>" + (x[i].AER * 100).toFixed(2) + "%" + "</em>" + "</td>";
+        }
+
+        $("#content").append(dynamic);
+
+        $("body").on('mouseover', 'p:not(.tooltipstered)', function () {
+            var rowData = ($(this).parent().parent().find('td:first').text());
+            console.log(rowData);
+            var currentColumnIndex = $(this).parent().index();
+            //console.log(currentColumnIndex);
+            var compare = "";
+            var features = "";
+            if (rowData.includes("Normalization")) {
+                compare = "None";
+            } else if (rowData.includes("CogToOrigin")) {
+                compare = "CogToOriginXY";
+            }
+            if (rowData.includes("XY")) {
+                features = "XY";
+            }
+            if (rowData.includes("XYP")) {
+                features = "XYP";
+            }
+            if (rowData.includes("CogToOrigin, XY")) {
+                Scaling = "None";
+            }
+            if (rowData.includes("CogToOrigin, XYP")) {
+                Scaling = "None";
+            }
+
+
+
+            var temp = data.filter(function (el) {
+                return el.Translation == compare &&
+                    el.Features == features &&
+                    el.ResamplingType_Filter == ResamplingTypeFilter &&
+                    el.Scaling == Scaling;
+            })
+            //console.log(temp);
+            /*var almost = "";
+            if(currentColumnIndex === 1){
+              almost = temp[Object.keys(temp)[Object.keys(temp).length-2]];
+            }
+            if(currentColumnIndex === 2){
+              almost = temp[Object.keys(temp)[Object.keys(temp).length-1]];
+            }*/
+
+            var rowData = ($(this).parent().text());
+            averageFAR = 0;
+            averageFRR = 0;
+            for (var i = 0; i < 6; i++) {
+                //averageAER += temp[i].AER * 100;
+                averageFAR += temp[i].FAR * 100;
+                averageFRR += temp[i].FRR * 100;
+            }
+
+            averageFAR = (averageFAR / uniqueDatabasesCount).toFixed(2);
+            averageFRR = (averageFRR / uniqueDatabasesCount).toFixed(2);
+
+            var test = "AER: " + rowData + "<br>" + "FAR: " + averageFAR + "%" + "<br>" + "FRR: " + averageFRR + "%";
+
+            /* for (var i = 0; i <= 2; i++) {
+               test += Object.keys(almost)[i] + ": " + almost[Object.keys(almost)[i]].toFixed(2) + "%" + "<br>";
+             }*/
+
+
+
+            tooltipInstance = $(this).tooltipster({
+                contentAsHTML: true,
+                content: test,
+                delay: 0,
+                theme: 'tooltipster-noir',
+            })
+
+            tooltipInstance.tooltipster('open');
         })
 
-        tooltipInstance.tooltipster('open');
-      })
+        $("body").on('mouseover', 'em:not(.tooltipstered)', function () {
+            var rowData = ($(this).parent().parent().find('td:first').text());
+            console.log(rowData);
+            var currentColumnIndex = $(this).parent().index() - 2;
+            console.log(currentColumnIndex);
 
-      // verifier's name in the first column
-      $("#myTable tbody tr td:first-child").on('mouseover', function () {
-        let tooltipInstance = null;
-        let temp = ($(this).html());
-        let sep = ": ";
-        let eol = "<br>";
-
-
-        let singleVerifier = data.results.find(asd => asd.verifierName === temp);
-        let firstColumn = "";
-
-        for (let i = 1; i <= 2; i++) {
-          firstColumn += Object.keys(singleVerifier)[i] + sep + singleVerifier[Object.keys(singleVerifier)[i]] + eol;
-        }
-
-        tooltipInstance = $(this).tooltipster({
-          contentAsHTML: true,
-          content: firstColumn,
-          delay: 0,
-          theme: 'tooltipster-default',
-          multiple: true
-        })
-        tooltipInstance.tooltipster('open');
-      })
-
-      // Average and Average Core datas
-      $("body").on('mouseover', 'p:not(.tooltipstered)', function () {
-        let rowData = ($(this).parent().parent().find('td:first').text());
-        let currentColumnIndex = $(this).parent().index();
+            var compare = "";
+            var features = "";
+            if (rowData === "Normalization, XY") {
+                compare = "None";
+                features = "XY";
+                Scaling = "X01Y01";
+            }
+            if (rowData === "Normalization, XYP") {
+                compare = "None";
+                features = "XYP";
+                Scaling = "X01Y01";
+            }
+            if (rowData === "CogToOrigin, XY") {
+                compare = "CogToOriginXY";
+                features = "XY";
+                Scaling = "None";
+            }
+            if (rowData === "CogToOrigin, XYP") {
+                compare = "CogToOriginXY";
+                features = "XYP";
+                Scaling = "None";
+            }
+            
 
 
-        let currentRow = data.results.find(asd => asd.verifierName === rowData);
-        let preCell = "";
-        if (currentColumnIndex === 1) {
-          preCell = currentRow[Object.keys(currentRow)[Object.keys(currentRow).length - 2]];
-        }
-        if (currentColumnIndex === 2) {
-          preCell = currentRow[Object.keys(currentRow)[Object.keys(currentRow).length - 1]];
-        }
+            var temp = data.filter(function (el) {
+                return el.Translation == compare &&
+                    el.Features == features &&
+                    el.ResamplingType_Filter == ResamplingTypeFilter &&
+                    el.Scaling == Scaling;
+            })
 
-        let content = "";
+            console.log(temp);
+            var db = temp[currentColumnIndex];
+            console.log(db);
+            /*var final = db[Object.keys(db)[0]];
 
-        for (let i = 0; i <= 2; i++) {
-          content += Object.keys(preCell)[i] + ": " + preCell[Object.keys(preCell)[i]].toFixed(2) + "%" + "<br>";
-        }
+            var temp = data.results.find(asd => asd.verifierName === rowData);
+            var almost = temp[Object.keys(temp)[currentColumnIndex + 3]];
 
 
 
-        tooltipInstance = $(this).tooltipster({
-          contentAsHTML: true,
-          content: content,
-          delay: 0,
-          theme: 'tooltipster-noir',
-        })
-
-        tooltipInstance.tooltipster('open');
-      })
-
-      // all cells related to databases
-      $("body").on('mouseover', 'em:not(.tooltipstered)', function () {
-        let rowData = ($(this).parent().parent().find('td:first').text());
-        let currentColumnIndex = $(this).parent().index() - 3;
-        let singleDB = data.databases[currentColumnIndex];
-        let firstValue = singleDB[Object.keys(singleDB)[0]];
-
-        let getDB = data.results.find(asd => asd.verifierName === rowData);
-        let currentData = getDB[Object.keys(getDB)[currentColumnIndex + 3]];
-
-        let tooltipInstance = null;
-
-        let row = data.results.find(asd => asd.verifierName === rowData);
-        let content = "";
-
-        content += Object.keys(row)[0] + ": " + row[Object.keys(row)[0]] + "<br>";
-        content += "databaseName: " + firstValue + "<br>";
+            var tooltipInstance = null;
 
 
-        for (let i = 0; i <= 2; i++) {
-          content += Object.keys(currentData)[i] + ": " + currentData[Object.keys(currentData)[i]].toFixed(2) + "%" + "<br>";
-        }
+            var row = data.results.find(asd => asd.verifierName === rowData);*/
+            var test = "";
 
-        if (Object.keys(currentData)[3] === 'Comment') {
-          content += Object.keys(currentData)[3] + ": " + currentData[Object.keys(currentData)[3]];
-        }
+            test += "verifierName" + ": " + rowData + "<br>";
+            test += "databaseName: " + db.Database + "<br>";
+            test += "AER: " + (db.AER*100).toFixed(2) + "%" + "<br>";
+            test += "FAR: " + (db.FAR*100).toFixed(2) + "%" + "<br>";
+            test += "FRR: " + (db.FRR*100).toFixed(2) + "%" + "<br>";
 
 
-        tooltipInstance = $(this).tooltipster({
-          contentAsHTML: true,
-          content: content,
-          delay: 0,
-          theme: 'tooltipster-punk'
+            /*for (var i = 0; i <= 2; i++) {
+                test += Object.keys(almost)[i] + ": " + almost[Object.keys(almost)[i]].toFixed(2) + "%" + "<br>";
+            }
+
+            if (Object.keys(almost)[3] === 'Comment') {
+                test += Object.keys(almost)[3] + ": " + almost[Object.keys(almost)[3]];
+            }*/
+
+
+            tooltipInstance = $(this).tooltipster({
+                contentAsHTML: true,
+                content: test,
+                delay: 0,
+                theme: 'tooltipster-punk'
+
+            })
+            tooltipInstance.tooltipster('open');
 
         })
-        tooltipInstance.tooltipster('open');
 
-      })
     }
-  });
+
+    oReq.send();
+
 });
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
